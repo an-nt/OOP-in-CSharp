075e3b626893cf1f697a3a56cf4e7b18301b16b5
an-nt <thienan175@gmail.com>
2021-01-08 13:07:39 +0700

    constructor - access modifier - getter setter

--- OOP/Diem.cs
+++ OOP/Diem.cs
@@ -6,9 +6,40 @@ namespace OOP
 {
     public class Diem
     {
-        public int X;
-        public int Y;
-
+        private int _X;
+        private int _Y;
+        public int X
+        {
+            get
+            {
+                return this._X;
+            }
+            set
+            {
+                this._X = value;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return this._Y;
+            }
+            set
+            {
+                this._Y = value;
+            }
+        }
+        public Diem()
+        {
+            this._X = 0;
+            this._Y = 0;
+        }
+        public Diem(int x, int y)
+        {
+            this._X = x;
+            this._Y = y;
+        }
         public void NhapDiem(string ThongBaoNhap)
         {
             Console.WriteLine(ThongBaoNhap);
@@ -18,12 +49,12 @@ namespace OOP
             Console.WriteLine("Nhap Y:");
             int y = int.Parse(Console.ReadLine());
             
-            this.X = x;
-            this.Y = y;
+            this._X = x;
+            this._Y = y;
         }
         public double TinhKhoangCanhDen(Diem diem)
         {
-            return Math.Sqrt((this.X - diem.X) * (this.X - diem.X) + (this.Y - diem.Y) * (this.Y - diem.Y));
+            return Math.Sqrt((this._X - diem._X) * (this._X - diem._X) + (this._Y - diem._Y) * (this._Y - diem._Y));
         }
     }
 }
